227943aa01d61656b292b93674f75041b7dc3638
trelea dev <treleamarius76@gmail.com>
2025-07-24 21:48:11 +0300

    shirts

--- src/shared/pipes/thumbnail.pipe.ts
+++ src/shared/pipes/thumbnail.pipe.ts
@@ -5,12 +5,12 @@ import {
 } from '@nestjs/common';
 
 export class ThumbnailValidationPipe implements PipeTransform {
-  constructor(private readonly size: number = 1024 * 1024 * 5) {}
+  constructor(private readonly size: number = 1024 * 1024 * 100) {}
 
   transform(thumbnail: Express.Multer.File, metadata: ArgumentMetadata) {
     if (!thumbnail) return;
     if (thumbnail.size > this.size)
-      throw new BadRequestException('Max File Size 1MB');
+      throw new BadRequestException('Max File Size 100MB');
 
     if (
       !['image/jpg', 'image/jpeg', 'image/png', 'image/webp'].includes(
