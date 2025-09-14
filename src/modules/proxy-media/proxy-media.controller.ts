f86602f0458e44cef1dad2556029b52151186d24
trelea dev <treleamarius76@gmail.com>
2025-09-15 00:33:50 +0300

    access origins

--- src/modules/proxy-media/proxy-media.controller.ts
+++ src/modules/proxy-media/proxy-media.controller.ts
@@ -1,17 +1,40 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('proxy-media')
 export class ProxyMediaController {
+  constructor(private readonly configService: ConfigService) {}
   @Get('')
-  async fetchImage(@Query('url') url: string, @Res() res: Response) {
+  async fetchImage(
+    @Query('url') url: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     try {
       const response = await fetch(url);
       const buffer = await response.arrayBuffer();
 
+      // Define your allowed origins
+      const allowedOrigins = this.configService
+        .getOrThrow<string>('ORIGINS')
+        .split(',');
+
+      // Get the request origin
+      const requestOrigin =
+        req.headers.origin ||
+        req.headers.referer?.split('/').slice(0, 3).join('/');
+
+      // Check if origin is allowed
+      const origin = allowedOrigins.includes(requestOrigin as string)
+        ? requestOrigin
+        : allowedOrigins[0];
+
       res.set({
         'Content-Type': response.headers.get('content-type') || 'image/jpeg',
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': 'true',
+        Vary: 'Origin', // Important for caching
         'Cache-Control': 'public, max-age=3600',
       });
 
